da142e2f03c27998096d7658e6daf537c8acd39c
GurnoorOberoi <gurnoor9965@gmail.com>
2026-01-12 15:18:49 +0530

    initial commit

---
+++ Day 6/GreetingApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using GreetingLibrary;
+namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name :");
+            string name = Console.ReadLine()!;
+            string result = GreetingHelper.GetGreeting(name);
+            Console.WriteLine(result);
+        }
+    }
+}
