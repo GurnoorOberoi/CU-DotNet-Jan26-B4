54499a1244dd8990fa552878a96ab9a2ee53092b
GurnoorOberoi <gurnoor9965@gmail.com>
2026-01-27 15:18:52 +0530

    commit

---
+++ Day 18 -02 Inheritance/Day 18 -02 Inheritance/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace Day_18__02_Inheritance
+{
+    class Employee
+    {
+        public Employee()
+        {
+            EmployeeId = 0;
+            EmployeeName = string.Empty;
+            BasicSalary = decimal.Zero;
+            ExperienceInYears = 0;
+        }
+        public Employee(int id, string name, decimal salary, int years)
+        {
+            EmployeeId = id;
+            EmployeeName = name;
+            BasicSalary = salary;
+            ExperienceInYears = years;
+        }
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public decimal BasicSalary { get; set; }
+        public int ExperienceInYears { get; set; }
+
+        public decimal CalculateAnnualSalary()
+        {
+            return BasicSalary * 12;
+        }
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"ID = {EmployeeId}");
+            Console.WriteLine($"Name = {EmployeeName}");
+            Console.WriteLine($"Annual Salary = {CalculateAnnualSalary()}");
+        }
+    }
+    class PermanentEmployee: Employee
+    {
+        public PermanentEmployee(int id, string name, decimal salary, int years): base(id, name, salary, years)
+        {
+        }
+        public new decimal CalculateAnnualSalary()
+        {
+            decimal HRA = BasicSalary * 0.2m;
+            decimal specialAllowance = BasicSalary * 0.1m;
+            decimal bonus = ExperienceInYears >= 5 ? 50000 : 0;
+            return (BasicSalary*12) + (HRA*12) + (specialAllowance*12)+ bonus; 
+        }
+    }
+    class ContractEmployee : Employee
+    {
+        public ContractEmployee(int id, string name, decimal salary, int years , int duration): base (id, name, salary, years)
+        {
+            ContractDurationInMonths = duration;
+        }
+        public int ContractDurationInMonths  { get; set; }
+        public new decimal CalculateAnnualSalary()
+        {
+            decimal bonus = ContractDurationInMonths >= 12 ? 30000 : 0;
+            return (BasicSalary * 12)+bonus;
+        }
+
+    }
+    class InternEmployee : Employee
+    {
+        public InternEmployee(int id, string name, decimal salary, int years):base (id, name, salary, years)
+        {
+        }
+        public new decimal CalculateAnnualSalary()
+        {
+            return BasicSalary * 12;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee(1, "E1", 3000, 5);
+            PermanentEmployee e2 = new PermanentEmployee(2, "E2", 3000, 5);
+            ContractEmployee e3 = new ContractEmployee(3, "E3", 4550.85m, 9, 8);
+            InternEmployee e4 = new InternEmployee(4, "E4", 9500, 3);
+            Employee e5 = new PermanentEmployee(5, "E5", 8500, 2);
+            Console.WriteLine("Employee Compensation Management System");
+            Console.WriteLine("\nEmployee Base Method");
+            Console.WriteLine(e1.CalculateAnnualSalary().ToString("N2"));
+            Console.WriteLine("\nPermanent Derived Method");
+            Console.WriteLine(e2.CalculateAnnualSalary().ToString("N2"));
+            Console.WriteLine("\nCOntract Derived Method");
+            Console.WriteLine(e3.CalculateAnnualSalary().ToString("N2"));
+            Console.WriteLine("\nInter Derived Method");
+            Console.WriteLine(e4.CalculateAnnualSalary().ToString("N2"));
+            Console.WriteLine("\nBase class references");
+            Console.WriteLine(e5.CalculateAnnualSalary().ToString("N2"));
+            Console.WriteLine();
+            e1.DisplayEmployeeDetails();
+        }
+    }
+}
---
+++ Day 18-01/Day 18-01/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Day_18_01
+{
+    class Loan
+    {
+        public Loan()
+        {
+            LoanNumber = string.Empty;
+            CustomerName = string.Empty;
+            PrincipalAmount = decimal.Zero;
+            TenureInYears = 0;
+        }
+        public Loan(string number, string name, decimal amount, int years)
+        {
+            LoanNumber = number;
+            CustomerName = name;
+            PrincipalAmount = amount;
+            TenureInYears = years;
+        }
+        public string LoanNumber { get; set; }
+        public string CustomerName { get; set; }
+        public decimal PrincipalAmount { get; set; }
+        public int TenureInYears { get; set; }
+
+        public double CalculateEMI()
+        {
+            return (double)PrincipalAmount * 0.1 * TenureInYears;
+        }
+
+    }
+    class HomeLoan : Loan
+    {
+        public HomeLoan(string number, string name, decimal amount, int years) : base(number, name, amount, years)
+        {
+
+        }
+        public new double CalculateEMI()
+        {
+            return ((double)PrincipalAmount * 0.01) * 0.08 * TenureInYears;
+        }
+    }
+    class CarLoan : Loan
+    {
+        public CarLoan(string number, string name, decimal amount, int years) : base(number, name, amount, years)
+        {
+
+        }
+        public new double CalculateEMI()
+        {
+            return ((double)PrincipalAmount + 15000) * 0.09 * TenureInYears;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Loan[] loans = new Loan[4]
+       {
+            new HomeLoan("1","S1",4500.50m,7),
+            new HomeLoan("2","S2",3500.50m,10),
+            new CarLoan("1","C1",4550.50m,3),
+            new CarLoan("2","C1",5000,11)
+       };
+            for (int i = 0; i < loans.Length; i++)
+            {
+
+                Console.WriteLine(loans[i].CalculateEMI().ToString("N2"));
+            }
+        }
+    }
+}
