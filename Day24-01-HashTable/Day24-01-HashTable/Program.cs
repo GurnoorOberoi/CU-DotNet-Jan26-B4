6318223804d0aea6abfbfae10dc75cb615c93596
GurnoorOberoi <gurnoor9965@gmail.com>
2026-02-03 14:37:44 +0530

    commit

---
+++ Day24-01-HashTable/Day24-01-HashTable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace Day24_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add(101, "Alice");
+            ht.Add(102, "Bob");
+            ht.Add(103, "Charlie");
+            ht.Add(104, "Diana");
+            if (!ht.ContainsKey(105))
+            {
+                ht.Add(105, "Edward");
+            }
+            else
+            {
+                Console.WriteLine("ID already exists");
+            }
+
+            string name = (string)ht[102];
+            Console.WriteLine("Employee with ID 102 : " + name);
+            Console.WriteLine();
+            Console.WriteLine("Employee Details");
+            foreach (DictionaryEntry item in ht)
+            {
+                Console.WriteLine($"ID: {item.Key}, Name: {item.Value}");
+            }
+            Console.WriteLine();
+            ht.Remove(103);
+            Console.WriteLine("Total Employee : " + ht.Count);
+        }
+    }
+}
---
+++ Day24-02/Day24-02/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day24_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, string> leaderboard = new SortedDictionary<double, string>();
+            leaderboard.Add(55.42, "SwiftRacer");
+            leaderboard.Add(52.10, "SpeedDemon");
+            leaderboard.Add(58.91, "SteadyEddie");
+            leaderboard.Add(51.05, "TurboTom");
+            Console.WriteLine("Records of the Players");
+            foreach (var item in leaderboard)
+            {
+                Console.WriteLine($"Player's Name: {item.Value}, Times: {item.Key}");
+            }
+            Console.WriteLine();
+            Console.WriteLine( "First Entry: "+ leaderboard.First());
+            leaderboard.Remove(58.91);
+            Console.WriteLine();
+            leaderboard.Add(54.00, "SteadyEddie");
+            Console.WriteLine("\nUpdated Record");
+            foreach (var item in leaderboard)
+            {
+                Console.WriteLine($"Player's Name: {item.Value}, Times: {item.Key}");
+            }
+        }
+    }
+}
