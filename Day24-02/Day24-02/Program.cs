6b174a15ed90acf1388037b7c769d42bf95d030c
GurnoorOberoi <gurnoor9965@gmail.com>
2026-02-03 15:03:23 +0530

    commit

--- Day24-02/Day24-02/Program.cs
+++ Day24-02/Day24-02/Program.cs
@@ -10,14 +10,18 @@
             leaderboard.Add(58.91, "SteadyEddie");
             leaderboard.Add(51.05, "TurboTom");
             Console.WriteLine("Records of the Players");
+            double time =0;
             foreach (var item in leaderboard)
             {
                 Console.WriteLine($"Player's Name: {item.Value}, Times: {item.Key}");
+                if(item.Value == "SteadyEddie")
+                {
+                    time = item.Key;
+                }
             }
+            leaderboard.Remove(time);
             Console.WriteLine();
             Console.WriteLine( "First Entry: "+ leaderboard.First());
-            leaderboard.Remove(58.91);
-            Console.WriteLine();
             leaderboard.Add(54.00, "SteadyEddie");
             Console.WriteLine("\nUpdated Record");
             foreach (var item in leaderboard)
