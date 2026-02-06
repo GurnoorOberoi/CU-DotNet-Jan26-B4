2806ca580dfb380bc9d3b0be06bf790b0d8623fb
GurnoorOberoi <gurnoor9965@gmail.com>
2026-02-06 15:04:39 +0530

    commit

---
+++ Day27-01-DailyLogger/Day27-01-DailyLogger/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Day27_01_DailyLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory = @"..\..\..\";
+            if(!Directory.Exists(directory))
+            {
+                Console.WriteLine("Directory Does not exist");
+            }
+            string file = "journal.txt";
+            string path = directory + file;
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("File does not exist");
+            }
+            using StreamWriter sw = new StreamWriter(path,true);
+            do
+            {
+                Console.WriteLine("Enter the Reflection Data: ");
+                string data = Console.ReadLine();
+                if (string.IsNullOrEmpty(data) || string.IsNullOrWhiteSpace(data))
+                {
+                    break;
+                }
+                sw.WriteLine(data);
+            } while (true);
+        }
+    }
+}
---
+++ Day27-02-PortfolioFile/Day27-02-PortfolioFile/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Day27_02_PortfolioFile
+{
+    class Loan
+    {
+        public string ClientName { get; set; }
+        public double Principal { get; set; }
+        public double InterestRate { get; set; }
+        public double GetInterestAmount()
+        {
+            return Principal * InterestRate / 100;
+        }
+        public string GetRiskLevel()
+        {
+            if (InterestRate > 10)
+                return "High Risk";
+            else if (InterestRate >= 5)
+                return "Medium Risk";
+            else
+                return "Low Risk";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"..\..\..\Loan.csv";
+            Console.OutputEncoding= Encoding.UTF8;
+            Console.WriteLine("Enter the client name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the Principal: ");
+            double principal;
+            while (!double.TryParse(Console.ReadLine(), out principal))
+            {
+                Console.WriteLine("Invalid. Enter again");
+            }
+            Console.WriteLine("Enter the rate: ");
+            double rate;
+            while(!double.TryParse(Console.ReadLine(),out rate))
+            {
+                Console.WriteLine("Invalid. Enter again");
+            }
+            using (StreamWriter sw = new StreamWriter(file, true))
+            {
+                sw.WriteLine($"{name},{principal},{rate}");
+            };
+            List<Loan> l = new List<Loan>();
+            using StreamReader sr = new StreamReader(file);
+            sr.ReadLine();
+            string line;
+            while((line = sr.ReadLine())!=null)
+            {
+                string[] parts = line.Split(',');
+                if (parts.Length != 3)
+                    continue;
+                string Name = parts[0];
+                double.TryParse(parts[1], out principal);
+                double.TryParse(parts[2], out rate);
+                Loan loan = new Loan()
+                {
+                    ClientName = Name,
+                    Principal = principal,
+                    InterestRate = rate
+                };
+                l.Add(loan);
+            }
+            Console.WriteLine();
+            Console.WriteLine("The Loan Portfolio Manager");
+            Console.WriteLine();
+            Console.WriteLine($"{"Client",-15}|{"Principal",-15}|{"Interest",-15}|{"Risk"}");
+            Console.WriteLine("-----------------------------------------------------------");
+            foreach (var item in l)
+            {
+                Console.WriteLine($"{item.ClientName,-15}" +
+                    $"|{item.Principal,-15:C}" +
+                    $"|{item.GetInterestAmount(),-15:C}" +
+                    $"|{item.GetRiskLevel()}");
+            }
+        }
+    }
+}
