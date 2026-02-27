1d4edc0663cbcb3f01ed52f3706f2be60cc62051
GurnoorOberoi <gurnoor9965@gmail.com>
2026-02-27 23:42:26 +0530

    commit

---
+++ Exercises/Day13 Exercise/GymCharges/GymCharges/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace GymCharges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal amount = calculateGymMembership(Tread: false, Weightlifting: false, Zumba: false);
+
+            if (amount > 0)
+            {
+                Console.WriteLine($"Total Monthly Gym Fee (including GST): ₹{amount:F2}");
+            }
+        }
+        static decimal calculateGymMembership(bool Tread, bool Weightlifting, bool Zumba)
+        {
+            decimal fixedPrices = 1000;
+            decimal total = fixedPrices;
+            if (!Tread && !Weightlifting && !Zumba)
+            {
+                total += 200;
+                //Console.WriteLine("At least one service must be selected");
+                //return 0;
+            }
+            if (Tread)
+            {
+                total += 300;
+            }
+            if (Weightlifting)
+            {
+                total += 500;
+            }
+            if (Zumba)
+            {
+                total += 250;
+            }
+            decimal gst = total * 0.05m;
+            total += gst;
+            return total;
+        }
+    }
+}
---
+++ Exercises/Day13 Exercise/LineMethod/LineMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LineMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Display();
+            Display('+');
+            Display('$', 60);
+            printLine();
+            printLine(ch: '$');
+            printLine(ch: '$', num: 70);
+            printLine(75);
+            printLine(60, '+');
+        }
+        static void Display(char ch = '-', int num = 40)
+        {
+            Console.WriteLine(new string(ch, num));
+        }
+
+        static void printLine(int num = 40, char ch = '-')
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+        }
+    }
+}
