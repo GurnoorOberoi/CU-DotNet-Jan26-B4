b62ddae3735b51e6e26eed1d367ab0dc73449a1f
GurnoorOberoi <gurnoor9965@gmail.com>
2026-03-07 09:53:49 +0530

    commit

---
+++ Exercises/Day47 Exercise/TheCargoManifestOptimizer/TheCargoManifestOptimizer/Program.cs
@@ -0,0 +1,123 @@
+﻿namespace TheCargoManifestOptimizer
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public string Category { get; set; }
+        public Item(string name, double weight, string category)
+        {
+            Name = name;
+            Weight = weight;
+            Category = category;
+        }
+
+    }
+    public class Container
+    {
+        public string ContainerID { get; set; }
+        public List<Item> Items { get; set; }
+        public Container(string id, List<Item> item)
+        {
+            ContainerID = id;
+            Items = item;
+        }
+    }
+    public class CargoManifestOptimizer
+    {
+        private List<List<Container>> CargoBay;
+        public CargoManifestOptimizer(List<List<Container>> cargoBay)
+        {
+            this.CargoBay = cargoBay;
+        }
+        //Task A:
+        public List<String> FindHeavyContainers(double weightThreshold)
+        {
+            return CargoBay.SelectMany(r=>r).Where(c=>c.Items.Sum(i=>i.Weight)>weightThreshold).
+                Select(c=>c.ContainerID).OrderBy(c=>c).ToList();
+        }
+        //Task B
+        public Dictionary<string, int> GetItemCountsByCategory()
+        {
+            return CargoBay.Where(r=>r!=null).SelectMany(r=>r).SelectMany(c=>c.Items).GroupBy(i=>i.Category).
+                ToDictionary(g=>g.Key, g=>g.Count());
+        }
+        public List<Item> FlattenAndSortShipment()
+        {
+            return CargoBay.Where(r => r != null).SelectMany(r => r).SelectMany(c => c.Items).
+                GroupBy(i => i.Name).Select(g => g.First()).
+                OrderBy(i => i.Category).ThenByDescending(i => i.Weight).ToList();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cargoBay = new List<List<Container>>
+            {
+                new List<Container>
+                {
+                    new Container("C001", new List<Item>
+                    {
+                        new Item("Laptop", 2.5, "Tech"),
+                        new Item("Monitor", 5.0, "Tech"),
+                        new Item("Smartphone", 0.5, "Tech")
+                    }),
+                    new Container("C104", new List<Item>
+                    {
+                        new Item("Server Rack", 45.0, "Tech"), // Heavy Item
+                        new Item("Cables", 1.2, "Tech")
+                    })
+                },
+                // ROW 1: Mixed Consumer Goods
+                new List<Container>
+                {
+                    new Container("C002", new List<Item>
+                    {
+                        new Item("Apple", 0.2, "Food"),
+                        new Item("Banana", 0.2, "Food"),
+                        new Item("Milk", 1.0, "Food")
+                    }),
+                    new Container("C003", new List<Item>
+                    {
+                        new Item("Table", 15.0, "Furniture"),
+                        new Item("Chair", 7.5, "Furniture")
+                    })
+                },
+                // ROW 2: Fragile & Perishables (Includes an Empty Container)
+                new List<Container>
+                {
+                    new Container("C205", new List<Item>
+                    {
+                        new Item("Vase", 3.0, "Decor"),
+                        new Item("Mirror", 12.0, "Decor")
+                    }),
+                    new Container("C206", new List<Item>()) // EDGE CASE: Container with no items
+                },
+                // ROW 3: EDGE CASE - Empty Row
+                new List<Container>() // A row that exists but has no containers
+            };
+            var result = new CargoManifestOptimizer(cargoBay);
+            Console.WriteLine("Heavy Weight Container");
+            var heavyContainer = result.FindHeavyContainers(20);
+            foreach (var item in heavyContainer)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Item Count by Weight");
+            var ItemCount = result.GetItemCountsByCategory();
+            foreach (var item in ItemCount)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Flatten and Sort Shippment");
+            var SortShippment = result.FlattenAndSortShipment();
+            foreach (var item in SortShippment)
+            {
+                Console.WriteLine($"{item.Category} - {item.Name} - {item.Weight}");
+            }
+        }
+    }
+}
---
+++ Exercises/Day50 Exercise/StudentsDictionary/StudentsDictionary/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace StudentsDictionary
+{
+    class Student
+    {
+        public int Studid { get; set; }
+        public string SName { get; set; }
+        public Student(int id, string name)
+        {
+            Studid = id;
+            SName = name;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is Student s)
+            {
+                return this.Studid == s.Studid && this.SName == s.SName;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            //return Studid.GetHashCode();
+            return HashCode.Combine(Studid, SName);
+        }
+    }
+    class StudentManagement
+    {
+        static Dictionary<Student, int> dic = new Dictionary<Student, int>();
+        public static void AddorUpdateMarks(Student s, int marks)
+        {
+            if (dic.ContainsKey(s))
+            {
+                if (marks > dic[s])
+                {
+                    dic[s] = marks;
+                }
+
+            }
+            else
+            {
+                dic.Add(s, marks);
+                Console.WriteLine("Student added: " + s.SName);
+            }
+        }
+        public static void Display()
+        {
+            Console.WriteLine("\nStudent Records");
+            foreach (var item in dic)
+            {
+                Console.WriteLine($"ID: {item.Key.Studid} Name: {item.Key.SName} Marks: {item.Value}");
+            }
+        }
+    }
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Student S1 = new Student(1, "S1");
+            Student S2 = new Student(2, "S2");
+            StudentManagement.AddorUpdateMarks(S1, 75);
+            StudentManagement.AddorUpdateMarks(S2, 80);
+            StudentManagement.AddorUpdateMarks(new Student(1, "S1"), 90);
+            StudentManagement.Display();
+        }
+    }
+}
