0a30364fba9612381e1f3a45ac1c0c9368303325
GurnoorOberoi <gurnoor9965@gmail.com>
2026-03-14 12:49:49 +0530

    commit

---
+++ Assessment/Week10 Assessment/FinTrackPro/FinTrackPro/Models/Account.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinTrackPro.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Account Number")]
+        public int AccountNumber { get; set; }
+        [Required]
+        [Display(Name = "Account Holder Name")]
+        public string AccountName { get; set; }
+
+        [Required]
+        public double Balance { get; set; }
+
+        [ValidateNever]
+        public List<Transaction> Transactions { get; set; }
+    }
+}
---
+++ Assessment/Week10 Assessment/FinTrackPro/FinTrackPro/Models/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+
+namespace FinTrackPro.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public double Amount { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public int AccountId { get; set; }
+
+        // Navigation Property
+        [ValidateNever]
+        public Account Account { get; set; }
+    }
+}
