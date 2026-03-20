9ff3e2d59b04d478775d1e6004d50f7d148b248a
GurnoorOberoi <gurnoor9965@gmail.com>
2026-03-20 12:24:39 +0530

    Added Day 62-01 Exercise

---
+++ Exercises/Day62 Exercise/WebAPIEF/WebAPIEF/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAPIEF.Data;
+
+namespace WebAPIEF
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<WebAPIEFContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("WebAPIEFContext") ?? throw new InvalidOperationException("Connection string 'WebAPIEFContext' not found.")));
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
