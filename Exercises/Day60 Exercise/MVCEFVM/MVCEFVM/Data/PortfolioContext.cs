961e93dbd5f6e577a60902cc02a69450ff96c455
GurnoorOberoi <gurnoor9965@gmail.com>
2026-03-23 00:42:18 +0530

    Added Day 60 Exercise

---
+++ Exercises/Day60 Exercise/MVCEFVM/MVCEFVM/Data/PortfolioContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCEFVM.Models;
+
+    public class PortfolioContext : DbContext
+    {
+        public PortfolioContext (DbContextOptions<PortfolioContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCEFVM.Models.Investment> Investment { get; set; } = default!;
+    }
---
+++ Exercises/Day60 Exercise/MVCEFVM/MVCEFVM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCEFVM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
