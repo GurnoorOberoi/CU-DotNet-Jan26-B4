459f253cae5d544be15b1c70187deca932109ca8
GurnoorOberoi <gurnoor9965@gmail.com>
2026-04-18 12:52:20 +0530

    Added week 15 Assessment

---
+++ Assessment/Week15 Assessment/NorthwindCatalog/NorthwindCatalog.Web/Controllers/SummaryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NorthwindCatalog.DTOs;
+
+namespace NorthwindCatalog.Web.Controllers
+{
+    public class SummaryController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public SummaryController(IHttpClientFactory factory)
+        {
+            _client = factory.CreateClient("api");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _client.GetFromJsonAsync<List<CategorySummaryDto>>
+                ("api/ProductsApi/summary");
+
+            return View(data);
+        }
+    }
+}
